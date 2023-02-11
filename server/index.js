8d7659f6fc6355d1e390ec7e78f95a9dcfbb1772
mrefawaladam <aerefa55@gmail.com>
2023-02-11 21:36:41 +0700

    craete api register

--- server/index.js
+++ server/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express();
 const cros = require('cors')
+const mongoose = require('mongoose');
+const User = require('./models/user.model');
+
+mongoose.connect('mongodb://localhost:27017/auth')
 
 app.use(cros())
 app.use(express.json())
 
-app.post('/api/register', (req, res) => {
-    console.log(req.body)
-    res.json({status:'ok'});
+app.post('/api/register', async (req, res) => {
+	console.log(req.body)
+	try {
+		const newPassword = await bcrypt.hash(req.body.password, 10)
+		await User.create({
+			name: req.body.name,
+			email: req.body.email,
+			password: newPassword,
+		})
+		res.json({ status: 'ok' })
+	} catch (err) {
+		res.json({ status: 'error', error: 'Duplicate email' })
+	}
 })
 
 app.listen(1337,() =>{
